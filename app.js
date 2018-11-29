47bee3f15f1922b2a704fb26ba4a8d331eec1048
Jace Gold <jacegold@gmail.com>
2018-11-28 17:17:47 -0700

    Init Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Countdown to December 25th
+
+// HTML Element Grabber
+// Get date for today
+// The date we want (December 25th)
+// Calculation, to figure out the difference in time
+// Display on the HTML
+
+// eventlistener, onchange for the change of the difference
